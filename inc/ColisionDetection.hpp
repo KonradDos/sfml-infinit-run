c25dff7fe1e62d0415c8b11b9e668705cff48f57
konrad <konradniekras@gmail.com>
2018-04-26 12:41:22 +0200

    Added colision detection prototype and walls repositioning functions

---
+++ inc/ColisionDetection.hpp
@@ -0,0 +1,9 @@
+#ifndef ColisionDetection_hpp
+#define ColisionDetection_hpp
+
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+void colisionDetection(sf::RectangleShape &character, sf::RectangleShape &wall);
+
+#endif
---
+++ src/WallsRepositioning.cpp
@@ -0,0 +1,8 @@
+#include "WallsRepositioning.hpp"
+
+void wallsRepositioning(sf::RectangleShape &wall, const sf::RectangleShape & character, double windowWidth) {
+    
+    if(wall.getPosition().x < character.getPosition().x - 10)
+        wall.setPosition(character.getPosition().x + windowWidth + ((std::rand() % 1000 ) + 1) , 420);
+
+}
